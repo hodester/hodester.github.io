f9aebf51b8569bc843d86ec0c763856789fcae0f
Mark <smhodes03@gmail.com>
2017-04-01 15:06:36 -0700

    More updates

--- CIT261Team/Scripts/mapFuncs.js
+++ CIT261Team/Scripts/mapFuncs.js
@@ -2,10 +2,10 @@
 
 function changeMap(id) {
     var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
+    request.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var uriImg = '<img src="images/"+id+".png" alt="mapImage">'
-            // document.getElementById("map").innerHTML = uriImg;
+                // document.getElementById("map").innerHTML = uriImg;
             var parent = document.getElementById("map");
             var child = document.getElementById(id);
             // parent.removeChild(child);
@@ -16,3 +16,52 @@ function changeMap(id) {
     request.open("GET", 'images/' + id + ".png", true);
     request.send();
 }
+
+function line() {
+
+    var c = document.getElementById("altImageToSwap");
+    var ctx = c.getContext("2d");
+    ctx.lineWidth = 1;
+    var cr = 0;
+    var x = 0;
+    var frequency = 9;
+    var y = vertical;
+    var vertical = 58;
+    var slope = 15;
+    var m = 90 / 180 * Math.PI / frequency;
+
+    for (var i = 0; i <= 360; i += 10) {
+        ctx.moveTo(x, y);
+        x = i;
+        y = vertical - Math.sin(cr) * slope;
+        cr += m;
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+    //right();
+}
+
+// See UpdateSatPosition() in index.html
+// function right() {
+//     var redbox = document.getElementById("redbox");
+//     var interval = setInterval(frame, 20);
+//     var frequency = 300;
+//     var move = 0;
+//     var vertical = 58;
+//     var slope = 50;
+//     var cr = 0;
+//     var m = 90 / 180 * Math.PI / frequency;
+
+//     function frame() {
+//         for (i = 0; i <= 1; i++) {
+//             redbox.style.top = vertical - Math.sin(cr) * slope + "px";
+//             redbox.style.left = move + "px";
+//             cr += m;
+//             move++;
+//             if (move > 1000) {
+//                 clearInterval(interval);
+//                 break;
+//             }
+//         }
+//     }
+// }
